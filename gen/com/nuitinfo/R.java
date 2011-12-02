e361ff5ce7045be405a201d3d6e34b95af07ab78
Capelli Romain <gamerounet@hotmail.com>
2011-12-02 03:07:12 +0100

    event activity

--- gen/com/nuitinfo/R.java
+++ gen/com/nuitinfo/R.java
@@ -21,32 +21,34 @@ public final class R {
         public static final int profil=0x7f020004;
     }
     public static final class id {
-        public static final int add_friend_button=0x7f060003;
-        public static final int button_submit=0x7f060010;
+        public static final int add_friend_button=0x7f060004;
+        public static final int button_next=0x7f060003;
+        public static final int button_submit=0x7f060011;
         public static final int connectionAuto=0x7f060000;
         public static final int connection_button=0x7f060001;
-        public static final int editText_confirmpassword=0x7f06000e;
-        public static final int editText_email=0x7f06000f;
-        public static final int editText_password=0x7f06000d;
-        public static final int event_button=0x7f060007;
-        public static final int friend_button=0x7f060008;
-        public static final int layout_bot=0x7f060006;
-        public static final int layout_top=0x7f060005;
-        public static final int listView_people=0x7f060004;
-        public static final int param_button=0x7f060009;
-        public static final int profil_button=0x7f06000a;
+        public static final int editText_confirmpassword=0x7f06000f;
+        public static final int editText_email=0x7f060010;
+        public static final int editText_password=0x7f06000e;
+        public static final int event_button=0x7f060008;
+        public static final int friend_button=0x7f060009;
+        public static final int layout_bot=0x7f060007;
+        public static final int layout_top=0x7f060006;
+        public static final int listView_people=0x7f060005;
+        public static final int param_button=0x7f06000a;
+        public static final int profil_button=0x7f06000b;
         public static final int registration_button=0x7f060002;
-        public static final int sign_up_button=0x7f060011;
-        public static final int textView_birth=0x7f06000c;
-        public static final int textView_name=0x7f06000b;
+        public static final int sign_up_button=0x7f060012;
+        public static final int textView_birth=0x7f06000d;
+        public static final int textView_name=0x7f06000c;
     }
     public static final class layout {
         public static final int connection_activity=0x7f030000;
-        public static final int event_list_activity=0x7f030001;
-        public static final int friend_activity=0x7f030002;
-        public static final int home_activity=0x7f030003;
-        public static final int profil_activity=0x7f030004;
-        public static final int registration_activity=0x7f030005;
+        public static final int event_activity=0x7f030001;
+        public static final int event_list_activity=0x7f030002;
+        public static final int friend_activity=0x7f030003;
+        public static final int home_activity=0x7f030004;
+        public static final int profil_activity=0x7f030005;
+        public static final int registration_activity=0x7f030006;
     }
     public static final class string {
         public static final int add_friend=0x7f050000;
@@ -54,18 +56,23 @@ public final class R {
         public static final int connection=0x7f050002;
         public static final int connection_automatic=0x7f050003;
         public static final int connection_informations=0x7f050004;
-        public static final int email=0x7f050005;
-        public static final int first_name=0x7f050006;
-        public static final int identification=0x7f050007;
-        public static final int name=0x7f050008;
-        public static final int not_yet_registered=0x7f050009;
-        public static final int password=0x7f05000a;
-        public static final int personal_informations=0x7f05000b;
-        public static final int profil=0x7f05000c;
-        public static final int registration=0x7f05000d;
-        public static final int repeat_password=0x7f05000e;
-        public static final int sign_up=0x7f05000f;
-        public static final int submit=0x7f050010;
-        public static final int your_email=0x7f050011;
+        public static final int date=0x7f050005;
+        public static final int description=0x7f050006;
+        public static final int email=0x7f050007;
+        public static final int event=0x7f050008;
+        public static final int first_name=0x7f050009;
+        public static final int identification=0x7f05000a;
+        public static final int name=0x7f05000b;
+        public static final int next=0x7f05000c;
+        public static final int not_yet_registered=0x7f05000d;
+        public static final int password=0x7f05000e;
+        public static final int personal_informations=0x7f05000f;
+        public static final int profil=0x7f050010;
+        public static final int recipient=0x7f050011;
+        public static final int registration=0x7f050012;
+        public static final int repeat_password=0x7f050013;
+        public static final int sign_up=0x7f050014;
+        public static final int submit=0x7f050015;
+        public static final int your_email=0x7f050016;
     }
 }
