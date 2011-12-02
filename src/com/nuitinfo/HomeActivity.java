ea41e74abe36e9d97a97dd1dd7b591d999b7c5a5
Sebastien Fifre <fifre.sebastien@orange.fr>
2011-12-02 03:59:52 +0100

    events_tabs

--- src/com/nuitinfo/HomeActivity.java
+++ src/com/nuitinfo/HomeActivity.java
@@ -53,7 +53,10 @@ public class HomeActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(HomeActivity.this, "Not yet Implemented", 3000).show();
+				System.out.println("ICCCCCCCIIIIIII");
+				Intent intent = new Intent(HomeActivity.this,
+						EventActivity.class);
+				startActivity(intent);
 			}
 		});
         
